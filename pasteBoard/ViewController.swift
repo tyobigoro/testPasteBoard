774cc9642c00cea20c1532017326f08ef1686ce7
Tyobigoro <n.n.6.suke@gmail.com>
2018-04-07 12:59:47 +0900

    ｃ

--- pasteBoard/ViewController.swift
+++ pasteBoard/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //；；；；；；；
+       
         // ラベルに値を設定する
         sampleLabel.text = "sampleString"
     }
